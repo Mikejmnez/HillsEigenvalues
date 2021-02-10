a7ed4030ff793b62006416b68c884aab75d98442
Mikejmnez <mjimen17@jh.edu>
2021-02-10 11:46:32 -0500

    construct matrix for a given q value, not array

--- src/eig_system.jl
+++ src/eig_system.jl
@@ -1,8 +1,7 @@
-function matrix_construct(N::Int, qf::Float64)
-    q = collect(0:0.1:qf)*1im
+function matrix_construct(N::Int, q::Float64)
     d = [4r^2 for r in 0:N]
-    l = ones(N-1)
+    l = ones(N)
     l[1] = sqrt(2)
-    A = spdiagm(0 => d, 1 => q[end].*l, -1=> q[end].*l)
+    A = spdiagm(0 => d, 1 => q.*l, -1=> q.*l)
     return A
 end
