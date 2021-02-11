02ba946729420173ffe84b316ced9bd9b02fe41b
Mikejmnez <mjimen17@jh.edu>
2021-02-10 22:10:27 -0500

    added q-dependence

--- src/eig_system.jl
+++ src/eig_system.jl
@@ -9,16 +9,23 @@ end
 
 function eigval_iterator(N::Int64, q::Array{Complex{Float64}, 1})
     if q[1] == 0
-        q[1] = 0.5*(q[1]+q[2])
+        q[1] = 0.5*(q[1]+q[2]);
     end
-    A = matrix_construct(N, q[1])
-    l, = eigs(A, which=:SM)
-    a = l[1]
+    A = matrix_construct(N, q[1]);
+    l, = eigs(A, nev= N-1, which=:SM);
+    l = round.(l, digits=2);
+    I = sortperm(l, lt = (x,y) -> real(x)==real(y) ? imag(x)<imag(y) : real(x)<real(y));
+    a = l[I];
     for qf in q[2:end]
-        A = matrix_construct(N, qf)
-        l, = eigs(A, which=:SM)
-        a = union(a, l[1])
+        A = matrix_construct(N, qf);
+        l, = eigs(A, nev=N-1, which=:SM);
+        l = round.(l, digits=2);
+        I = sortperm(l, lt = (x,y) -> real(x)==real(y) ? imag(x)<imag(y) : real(x)<real(y));
+        a = hcat(a, l[I]);
     end
-    vals = Dict("a0" => a, "q" => q)
+    vals = Dict("a0" => a[1, :], "q"=> q);
+    for n in 1:length(l)-1
+        vals["a"*string(2n)] = a[n+1, :];
+    end
     return vals
 end
