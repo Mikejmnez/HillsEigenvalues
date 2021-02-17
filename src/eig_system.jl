867a02f0b58ce7ae91c631c5c4a79ed9b11b1895
Mikejmnez <mjimen17@jh.edu>
2021-02-16 22:24:02 -0500

    pass flag so that eigen-system matches Mathieu's

--- src/eig_system.jl
+++ src/eig_system.jl
@@ -1,8 +1,8 @@
-function matrix_construct(N::Int64, q::Complex{Float64}, alphas::Array{Float64, 1})
+function matrix_construct(N::Int64, q::Complex{Float64}, alphas::Array{Float64, 1}; flag::String="False")
     d = [4r^2 for r in 0:N]; # N+1 array
     A = spdiagm(0 => d);
     l = ones(N);
-    if length(alphas) == 1  # This is Mathieu's canonical form. Or if first element is non-zero
+    if flag == "True"  # This is Mathieu's canonical form. Or if first element is non-zero
         l[1] = sqrt(2) * l[1];
     end
     for i in 0:length(alphas[2:end])
@@ -14,17 +14,17 @@ function matrix_construct(N::Int64, q::Complex{Float64}, alphas::Array{Float64, 
     return A
 end
 
-function eigval_iterator(N::Int64, q::Array{Complex{Float64}, 1}, alphas::Array{Float64, 1})
+function eigval_iterator(N::Int64, q::Array{Complex{Float64}, 1}, alphas::Array{Float64, 1}; flag::String="False")
     if q[1] == 0
         q[1] = 0.5*(q[1]+q[2]);
     end
-    A = matrix_construct(N, q[1], alphas);
+    A = matrix_construct(N, q[1], alphas; flag);
     l, = eigs(A, nev= N-1, which=:SM);
     l = round.(l, digits=2);
     I = sortperm(l, lt = (x,y) -> real(x)==real(y) ? imag(x)<imag(y) : real(x)<real(y));
     a = l[I];
     for qf in q[2:end]
-        A = matrix_construct(N, qf, alphas);
+        A = matrix_construct(N, qf, alphas; flag);
         l, = eigs(A, nev=N-1, which=:SM);
         l = round.(l, digits=2);
         I = sortperm(l, lt = (x,y) -> real(x)==real(y) ? imag(x)<imag(y) : real(x)<real(y));
